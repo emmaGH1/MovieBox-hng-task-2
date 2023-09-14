5f55294485c1a85d43ea285fd424df9896998a78
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-09-14 04:23:17 +0100

    feat: Create api endpoint for featured-movies

---
+++ app/api/featured-movies/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server'
+const base_url = 'https://api.themoviedb.org/3/'
+
+export async function GET() {
+  const options = {
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.AUTHENTICATION_KEY}`
+    },
+     
+  };
+  
+  const res = await fetch(`${base_url}/movie/top_rated?language=en-US&page=1`, options)
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+    const data = await res.json()
+ 
+    return NextResponse.json({ data })
+}
+
+
+
