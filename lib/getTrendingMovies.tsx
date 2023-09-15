acf71f9716524baa0e24506623dcae130af870f5
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-09-15 23:23:33 +0100

    feat: Globalize apiKey import

--- lib/getTrendingMovies.tsx
+++ lib/getTrendingMovies.tsx
@@ -1,5 +1,4 @@
-import { BASE_URL } from "@/constants"
-const apiKey = process.env.API_KEY
+import { BASE_URL, apiKey } from "@/constants"
 
 export async function getTrendingMovies() {
     const res = await fetch(`${BASE_URL}/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`, {
