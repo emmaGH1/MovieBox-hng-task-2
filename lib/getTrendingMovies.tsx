fcace30a36ec3eddadea5c27b83ed0782d6c14dc
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-09-17 07:25:56 +0100

    feat: Deadline push

--- lib/getTrendingMovies.tsx
+++ lib/getTrendingMovies.tsx
@@ -1,7 +1,7 @@
 import { BASE_URL, apiKey } from "@/constants"
 
 export async function getTrendingMovies() {
-    const res = await fetch(`${BASE_URL}/movie/popular?api_key=${apiKey}&language=en-US&page=1`, {
+    const res = await fetch(`${BASE_URL}/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`, {
         cache: 'default',
     })
     
